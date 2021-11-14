eb78c455322da789352c18966fd58c79f8650989
Michael Bykov <m.bykov@gmail.com>
2021-11-14 22:47:57 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const log = console.log
+
+import path  from 'path'
+import _  from 'lodash'
+import {oxia, comb, plain, strip} from 'orthos'
+
+import { accents, scrape } from './lib/utils.js'
+import { getFlexes } from './lib/remote.js'
+
+let wordform = process.argv.slice(2)[0] //  'ἀργυρῷ'
+
+
+start(wordform)
+
+function start (wf) {
+    let cwf = comb(wf)
+    log('_start', cwf)
+    let flakes = scrape(cwf)
+    let tails = flakes.map(flake=> flake.tail)
+    log('_tails', tails)
+    let flexes = getFlexes(tails)
+    log('_flexes', flexes)
+}
