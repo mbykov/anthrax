12f5cee5c20b2b762e85dec8d89bb9e0258c6199
Michael Bykov <m.bykov@gmail.com>
2021-11-19 18:56:57 +0300

    v.1.0.0

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ async function start (wf) {
         log('_sgms', ids)
     })
 
-    let cleans = chains.map(chain=> filters(chain)).filter(chain=> chain.length).map(clean=> clean.filter(wf=> wf.flexes.length))
+    let cleans = chains.map(chain=> filters(chain)).filter(chain=> chain.length)
     cleans.forEach(clean=> {
         log('_clean', clean)
     })
@@ -54,45 +54,45 @@ async function start (wf) {
 
 // вопросы: εἰσαγγέλλω - два одинаковых sgms, ἐξαγγέλλω - то же
 
-
 async function dagging(chains, pcwf, head, flexes) {
-    /* if (pcwf == 'κρατια') log('_xx-head_KRAT', head) */
-    let flakes = scrape(pcwf)
-    let heads = flakes.map(flake=> flake.head)
-    /* if (pcwf == 'κρατια') log('_heads_KRAT', heads) */
-    let segments = await getSegments(heads)
-    /* if (pcwf == 'ἀγαπαω') log('_segments_KRAT', segments[0]) */
-    for await (let seg of segments) {
-        /* if (seg._id.length < 2) return */
-        let full = false
-        /* let headsrt = head.map(seg=> seg._id).join('') || '' */
-        flexes.forEach(flex=> {
-            if (pcwf == seg._id + plain(flex._id)) {
-                let chain = [seg, flex]
-                if (head.length) chain.unshift(...head)
-                full = true
-                /* if (pcwf == 'κρατια') log('_SEGM_KRAT', chain) */
-                if (!dag[pcwf]) dag[pcwf] = [seg, flex]
-                chains.push(chain)
-            }
-        })
-        if (full) continue
-        let pseg = plain(seg._id)
-        let repseg = new RegExp('^'+pseg)
-        let tail = pcwf.replace(repseg, '')
-        await diveDag(chains, seg, tail, head, flexes)
+  /* if (pcwf == 'κρατια') log('_xx-head_KRAT', head) */
+  let flakes = scrape(pcwf)
+  let heads = flakes.map(flake=> flake.head)
+  if (pcwf == 'οχος') log('_heads_OHOS', heads)
+  let segments = await getSegments(heads)
+  // if (pcwf == 'οχος') log('_segments_OH', segments[0])
+  for await (let seg of segments) {
+    /* if (seg._id.length < 2) return */
+    let full = false
+    /* let headsrt = head.map(seg=> seg._id).join('') || '' */
+    flexes.forEach(flex=> {
+      if (pcwf == seg._id + plain(flex._id)) {
+        let chain = [seg, flex]
+        if (head.length) chain.unshift(...head)
+        full = true
+        /* if (pcwf == 'κρατια') log('_SEGM_KRAT', chain) */
+        if (!dag[pcwf]) dag[pcwf] = [seg, flex]
+        chains.push(chain)
+      }
+    })
+    if (full) continue
 
-        let next = tail[0]
-        if (!vowels.includes(next)) continue
+    let pseg = plain(seg._id)
+    let repseg = new RegExp('^'+pseg)
+    let tail = pcwf.replace(repseg, '')
+    await diveDag(chains, seg, tail, head, flexes)
 
-        pseg = pseg + next
-        repseg = new RegExp('^' + pseg)
-        tail = pcwf.replace(repseg, '')
-        if (tail == pcwf) continue
-        await diveVowel(chains, seg, tail, head, next, flexes)
+    let next = tail[0]
+    if (!vowels.includes(next)) continue
 
-        // todo: -si- ??
-    }
+    pseg = pseg + next
+    repseg = new RegExp('^' + pseg)
+    tail = pcwf.replace(repseg, '')
+    if (tail == pcwf) continue
+    await diveVowel(chains, seg, tail, head, next, flexes)
+
+    // todo: -si- ??
+  }
 }
 
 async function diveDag(chains, seg, tail, head, flexes) {
